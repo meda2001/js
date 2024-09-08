5cfac208f5fc9f1d19b949ad438935fb5bf8c832
meda2001 <medamurariprasad2001@gmail.com>
2024-09-09 00:21:58 +0530

    variables, Data types , Primitive data types and Reference data types

--- index.js
+++ index.js
@@ -10,4 +10,74 @@
 
  // var is for older version 
 
- 
+/* data types  as primitive datatypes*/
+
+// numbers
+
+let a1 = 1
+let b = 2.3
+
+console.log("a1",a1,"and","b",b)
+
+console.log(typeof a1)
+console.log(typeof b)
+
+// strings 
+
+let name = "user123"
+
+let message = 'i am full stack developer'
+
+console.log(name);
+
+console.log(message);
+
+console.log(typeof name);
+console.log(typeof message)
+
+let char = 'a'
+console.log(char);
+console.log(typeof char)
+
+// booleans
+
+let isVisible = true ;
+
+console.log(isVisible);
+console.log(typeof isVisible)
+
+// undefined;
+
+let me;
+console.log(me);
+
+//  null  values 
+
+
+let userData = null;
+
+console.log(userData);
+console.log(typeof userData)
+
+/* datatypes as reference */
+
+// arrays 
+
+let myArray =[1,1.1,2,2.2]
+console.log(myArray, typeof myArray)
+
+let Array = ["user1","user2","user3",true]
+
+console.log(Array, typeof Array);
+
+// object is complex datatypes which is store in key and values pair format
+
+let myData = {
+    name : "userme",
+    age : 21
+}
+
+console.log(myData , typeof myData)
+
+console.log(myData.name , typeof myData.name)
+
